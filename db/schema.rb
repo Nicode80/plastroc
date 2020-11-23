1e6efc3bcff36131d34ae8a1382908ef526a3fa7
Thomas Le Bihan <thomas.66days@gmail.com>
2020-11-23 17:23:46 +0100

    tables + models finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_154943) do
+ActiveRecord::Schema.define(version: 2020_11_23_162200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_154943) do
   end
 
   create_table "missions", force: :cascade do |t|
-    t.string "status"
+    t.string "status", default: "ongoing"
     t.bigint "user_id", null: false
     t.bigint "package_id", null: false
     t.datetime "created_at", precision: 6, null: false
