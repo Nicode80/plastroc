2ab2471e58e2e253333a836f0bdffda3e1702b4f
Maximin Daudiffret <maximin.daudiffret@gmail.com>
2020-11-23 18:45:32 +0100

    db migration or admin user and material categ

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,5 +3,8 @@ class Material < ApplicationRecord
   has_many :campaigns
   has_one_attached :photo
 
+  CATEGORIES = ['paper', 'metal', 'plastic', 'wood', 'organic', 'fabric']
+
   validates :name, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_162200) do
+ActiveRecord::Schema.define(version: 2020_11_23_173901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_162200) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "missions", force: :cascade do |t|
@@ -115,6 +116,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_162200) do
     t.string "first_name"
     t.string "last_name"
     t.integer "xp", default: 0
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
