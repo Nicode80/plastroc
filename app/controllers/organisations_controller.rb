b1ba800a7bfe22283948f33f44fa908b9af9b592
Thomas Le Bihan <thomas.66days@gmail.com>
2020-11-24 10:56:25 +0100

    reinstall simple form without bootstrap

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,11 +1,28 @@
 class OrganisationsController < ApplicationController
 
+  def index
+    @organisations = policy_scope(Organisation)
+  end
+
   def new
     @organisation = Organisation.new
     authorize @organisation
   end
 
   def create
+    @organisation = Organisation.new(organisation_params)
+    @organisation.user = current_user
+    if @organisation.save!
+      redirect_to organisations_path
+    else
+      render :new
+    end
+    authorize @organisation
+  end
 
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :address, :city, :postcode, :about, :opening_hours, :contact, :photo)
   end
 end
