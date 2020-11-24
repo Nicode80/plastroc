923ed409c2bbd13386fabb887c5a142cee41a751
JR Lherm <lherm.jr@gmail.com>
2020-11-24 23:49:33 +0100

    Install sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,30 @@ import { initAutoFadeAlert } from "../plugins/auto_fade_alert"
 
 // Internal imports, e.g:
 import { initSelect2 } from '../components/init_select2';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSelect2();
   initAutoFadeAlert();
+});
+
+
+initSweetalert('#sweet-alert-demo', {
+    title: "A nice alert",
+    text: "This is a great alert, isn't it?",
+    icon: "success"
+}, (value) => {
+    console.log(value);
+});
+
+initSweetalert('#delete-organisation', {
+    title: "Delete",
+    text: "Are you sure you want to delete organisation ?",
+    icon: "error"
+}, (value) => {
+    if (value) {
+        // const link = document.getElementById('#delete-orga');
+        document.getElementById('delete-orga').click()
+    }
 });
