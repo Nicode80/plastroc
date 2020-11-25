dcb19b2b7e77e36e62177cd6569dcbfea6f2fd6c
JR Lherm <lherm.jr@gmail.com>
2020-11-25 09:15:33 +0100

    setup sweetalert

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,12 +1,14 @@
 import swal from 'sweetalert';
 
 const initSweetalert = (selector, options = {}, callback = () => {}) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
+  const elements = document.querySelectorAll(selector);
+  elements.forEach((swalButton) => {
+    if (swalButton) { // protect other pages
     swalButton.addEventListener('click', () => {
-      swal(options).then(callback);
+        swal(options).then(callback);
     });
-  }
+    }
+  })
 };
 
 export { initSweetalert };
