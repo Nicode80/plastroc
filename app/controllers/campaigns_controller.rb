c86291c5bed6a2b7d38533342fab459fbb6601bd
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-28 22:44:44 +0100

    guard close if min package nil

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -86,6 +86,9 @@ class CampaignsController < ApplicationController
   end
 
   def create_packages
+    if @campaign.min_package.nil?
+      @campaign.min_package = 1
+    end
     iterators = [(@campaign.target / @campaign.min_package).floor, 4].min
     names = ['Corail', 'Tortue', 'Dauphin', 'Baleine']
     x = 0
