67b13d3cf217b1d9bf898bc03217c399b9c9b167
Thomas Le Bihan <thomas.66days@gmail.com>
2020-11-29 19:28:58 +0100

    styling list toggle

--- app/javascript/components/confirmation_toggle.js
+++ app/javascript/components/confirmation_toggle.js
@@ -1,5 +1,7 @@
 const toggleConfirmation = () => {
   const checkbox = document.querySelector('#confirm');
+  const checkList = document.querySelector('#engagements');
+  const chekItems = document.querySelectorAll('#engagements li');
   const btn = document.querySelector('#btn-confirm');
   btn.disabled = true;
   // const checkQuantity = document.querySelector('#check-quantity');
@@ -9,13 +11,22 @@ const toggleConfirmation = () => {
 
   if(!checkbox) return;
 
+
   checkbox.addEventListener('change', (e) => {
     if(e.target.checked) {
       btn.classList.toggle('disabled');
       btn.disabled = false;
+      checkList.style.listStyle = 'none';
+      chekItems.forEach(item => {
+        item.classList.add('icon', 'check-green');
+      });
     } else {
       btn.classList.toggle('disabled');
       btn.disabled = true;
+      checkList.style.listStyle = '';
+      chekItems.forEach(item => {
+        item.classList.remove('icon', 'check-green');
+      });
     }
   });
 
