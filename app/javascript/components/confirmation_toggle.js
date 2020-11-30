97bc8e42b9d983da62e2ec5b99faf8fc86a14abe
Thomas Le Bihan <thomas.66days@gmail.com>
2020-11-30 17:18:08 +0100

    added color to differenciate package badge

--- app/javascript/components/confirmation_toggle.js
+++ app/javascript/components/confirmation_toggle.js
@@ -1,7 +1,7 @@
 const toggleConfirmation = () => {
   const checkbox = document.querySelector('#confirm');
   const checkList = document.querySelector('#engagements');
-  const chekItems = document.querySelectorAll('#engagements li');
+  const checkItems = document.querySelectorAll('#engagements li');
   const btn = document.querySelector('#btn-confirm');
 
   if(!checkbox) return;
@@ -12,14 +12,14 @@ const toggleConfirmation = () => {
       btn.classList.toggle('disabled');
       btn.disabled = false;
       checkList.style.listStyle = 'none';
-      chekItems.forEach(item => {
+      checkItems.forEach(item => {
         item.classList.add('icon', 'check-green');
       });
     } else {
       btn.classList.toggle('disabled');
       btn.disabled = true;
       checkList.style.listStyle = '';
-      chekItems.forEach(item => {
+      checkItems.forEach(item => {
         item.classList.remove('icon', 'check-green');
       });
     }
