a95b0952a10c95ad1d4d82b2ce99ee824064a688
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-30 15:40:46 +0100

    correct guard close to JS

--- app/javascript/components/sliding_card.js
+++ app/javascript/components/sliding_card.js
@@ -1,17 +1,20 @@
 const slidingCard = () => {
 
   const ctaButton = document.getElementById('CTA-join-campaign');
-  const arrow = ctaButton.querySelector('.fa-angle-double-up');
+  const arrow = document.querySelector('#CTA-join-campaign .fa-angle-double-up');
   const slidingCard = document.getElementById('sliding-card');
   const bottomText = document.getElementById('bottom-text');
-  if(!ctaButton) return;
 
-  ctaButton.addEventListener("click", (event) => {
-    slidingCard.classList.toggle("text-open");
-    bottomText.classList.toggle("text-open");
-    ctaButton.classList.toggle("plastroc-button-sticky-bottom");
-    arrow.classList.toggle('rotate-180');
-  });
+  if(ctaButton) {
+    ctaButton.addEventListener("click", (event) => {
+      slidingCard.classList.toggle("text-open");
+      bottomText.classList.toggle("text-open");
+      ctaButton.classList.toggle("plastroc-button-sticky-bottom");
+      arrow.classList.toggle('rotate-180');
+    });
+
+  }
+
 }
 
 export { slidingCard }
