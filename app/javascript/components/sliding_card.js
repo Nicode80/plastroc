3d2734a3a073268342306492691a24cd17b79488
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-30 17:20:50 +0100

    cleaner guard close

--- app/javascript/components/sliding_card.js
+++ app/javascript/components/sliding_card.js
@@ -1,19 +1,18 @@
 const slidingCard = () => {
 
   const ctaButton = document.getElementById('CTA-join-campaign');
-  const arrow = document.querySelector('#CTA-join-campaign .fa-angle-double-up');
+  if(!ctaButton) return;
+
+  const arrow = ctaButton.querySelector('.fa-angle-double-up');
   const slidingCard = document.getElementById('sliding-card');
   const bottomText = document.getElementById('bottom-text');
 
-  if(ctaButton) {
-    ctaButton.addEventListener("click", (event) => {
-      slidingCard.classList.toggle("text-open");
-      bottomText.classList.toggle("text-open");
-      ctaButton.classList.toggle("plastroc-button-sticky-bottom");
-      arrow.classList.toggle('rotate-180');
-    });
-
-  }
+  ctaButton.addEventListener("click", (event) => {
+    slidingCard.classList.toggle("text-open");
+    bottomText.classList.toggle("text-open");
+    ctaButton.classList.toggle("plastroc-button-sticky-bottom");
+    arrow.classList.toggle('rotate-180');
+  });
 
 }
 
