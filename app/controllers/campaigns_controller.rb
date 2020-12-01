b15bb5a0ae86224057252d90cd799bf98feec125
Thomas Le Bihan <thomas.66days@gmail.com>
2020-12-01 10:56:03 +0100

    Contextualized campaign show for sorters with ongoing missions

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -24,6 +24,7 @@ class CampaignsController < ApplicationController
     @volume_done = done_missions_calcul
     @ratio = @volume_done.fdiv(@campaign.target) * 100 # used in dataset for animated bar
     @citation = Citation.all.sample
+    @sorter_campaigns = current_user.packages.map { |pack| pack.campaign }
 
     authorize @campaign
   end
