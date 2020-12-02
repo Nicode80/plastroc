96d2d499fbab3200679dc78dbbbac0f2659b68bf
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-12-02 12:59:47 +0100

    get rid of console.log

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -103,7 +103,6 @@ const toggleCardHighlighting = (event) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  console.log(bounds);
   map.fitBounds(bounds, {padding: 70, duration: 0});
 };
 
