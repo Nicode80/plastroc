9ef616bb535c841b52a1ccd055ece160618fb34e
Thomas Le Bihan <thomas.66days@gmail.com>
2020-12-02 12:16:09 +0100

    refined modal layout

--- app/javascript/components/close_achivement_modal.js
+++ app/javascript/components/close_achivement_modal.js
@@ -4,14 +4,6 @@ const closeModal = () => {
   const closeButton = document.querySelectorAll('.close-modal')
   const modalContainer = document.querySelector('#modal-container');
 
-  // modalBody.onclick = function(e) {
-  //   if(e.target != document.getElementById('content-area')) {
-  //       console.log('You clicked outside');
-  //   } else {
-  //       console.log('You clicked inside');
-  //   }
-  // }
-
   closeButton.forEach((button) => {
     button.addEventListener('click', (e) => {
       modalBody.classList.remove('achivement-modal');
